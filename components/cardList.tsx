562aded4ec2474eda5c541fbb10dc2d2f991bd19
aleksandrova22 <lialia1986@mail.ru>
2025-11-07 21:27:56 +0700

    0711++

--- components/cardList.tsx
+++ components/cardList.tsx
@@ -1,8 +1,8 @@
 
 'use client';
-import {useState } from "react";
+import { useState } from "react";
 
-import { useDispatch, useSelector } from "react-redux";
+import {  useSelector } from "react-redux";
 import { CardItem } from "./cardItem";
 // import { Card } from "@/types/card";
 import { RootState } from "@/redux/store";
@@ -11,15 +11,14 @@ import styles from './cardList.module.css';
 
 export function CardList() {
 
-    const [filter, setFilter] = useState<'all' | 'favorites'>('all'); // Состояние для фильтра
+    const [filter, setFilter] = useState<'all' | 'favorites'>('all');
     // const dispatch = useDispatch();
-    const [searchQuery, setSearchQuery] = useState(''); // Добавлено: состояние для поиска
-    // Получаем все карточки и избранные из Redux
+    const [searchQuery, setSearchQuery] = useState('');
     const allCards = useSelector((state: RootState) => state.cards.items);
     const favorites = useSelector((state: RootState) => state.favorites.items);
 
 
-    // Фильтруем данные: если 'favorites', показываем только избранные, иначе все
+    // Фильтр : если 'favorites', показываем только избранные, иначе все
     let updatedData = filter === 'favorites' ? favorites : allCards;
 
     if (searchQuery.trim()) {
@@ -31,18 +30,18 @@ export function CardList() {
     return <>
         <div >
 
-            <h2 className={styles.h2}>Картинки</h2>
+            <h2 className={styles.h2}>Продукты</h2>
 
             <div className={styles.input}>
                 <input
                     type="text"
-                    placeholder="Поиск по названию..."
+                    placeholder="Введите название"
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
                     style={{ marginBottom: '10px', padding: '8px', width: '100%', border: '1px solid #ccc', borderRadius: '4px' }}
                 />
             </div>
-            <button onClick={() => setFilter('all')} className={`${styles.filterButton} ${filter === 'all' ? styles.active : ''}`} >Все карточки</button>
+            <button onClick={() => setFilter('all')} className={`${styles.filterButton} ${filter === 'all' ? styles.active : ''}`} >Все продукты</button>
             <button onClick={() => setFilter('favorites')}>Избранные</button>
 
             {
@@ -51,7 +50,7 @@ export function CardList() {
                         <CardItem key={elem.id} card={elem} />)
 
                 ) : (
-                    <p>Карточки для отображения отсутствуют</p>
+                    <p>Продукты для отображения отсутствуют</p>
                 )
 
             }
--- db.jsx
+++ db.jsx
@@ -5,14 +5,14 @@ export const cards = [
     {
         id: 1,
         title: "card1",
-        imageUrl: "https://downloader.disk.yandex.ru/preview/583a06d703942d48feded577c3954b862860db7222c8910eed4d5dc1129527d2/690e2eac/U2HK7drpNcK702CKquVIJoYNZE_HnJ5cxUMtDxkYpIY92vWEHSOdofGssHhnya6BWGHW5LBGtys7nW5kJc2WlA%3D%3D?uid=0&filename=breakfast.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v3&size=2048x2048",
+        imageUrl: "lhttps://images.wallpaperscraft.ru/image/single/vishni_iagody_krasnyj_160567_300x168.jpg",
         description: "описание вкусный завтрак"
 
     },
     {
         id: 2,
         title: "card2",
-        imageUrl: "https://4.downloader.disk.yandex.ru/preview/370d86133b029e160ce6629e7a1bc9d2a7accaadd3a6b1212171f95e24f761dc/inf/PZNomX4NqyE_Nk8wuk9ZcGSw8NiDDnc1AF4avBWb5JR34aPVLOkLvHK32vC1cJ40V5s8hwAhU2kgawrw5kU_WQ%3D%3D?uid=2187528810&filename=22.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=2187528810&tknv=v3&size=1905x919",
+        imageUrl: "https://images.wallpaperscraft.ru/image/single/zavtrak_frukty_klubnika_174810_300x168.jpg",
         description: "описание вкусный завтрак сосиски хлеб масло"
 
     },
@@ -21,7 +21,7 @@ export const cards = [
 
         id: 3,
         title: "card3",
-        imageUrl: "https://downloader.disk.yandex.ru/preview/d7acd961fcf2e70c24ecc9cbde78aea3c6e8593350fb863346336c2e28247178/690e2f0a/AX8htIaZGosvs1xfjSI1aeUArJN9K5l7qlZdB4-eGXGzDedfjoX9MimXiSMqOB7rXVOYwFeizM6nqqUIR7Kb-w%3D%3D?uid=0&filename=44.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v3&size=2048x2048",
+        imageUrl: "https://images.wallpaperscraft.ru/image/single/shokolad_orehi_sladosti_198186_300x168.jpg",
         description: "описание вкусный завтрак панкейки"
     },
 
@@ -29,7 +29,7 @@ export const cards = [
     {
         id: 4,
         title: "card4",
-        imageUrl: "https://downloader.disk.yandex.ru/preview/1df9c260e00f945c8715e9df9a93d6564dd0fbcd750fd488e5a05b483d4f5942/690e2ed2/L38vJlvUKBQxj-slNfGqcOu7PXHpBHgnMsQ50wvqvtfSUqvQ-aQWKROBRwso73qRzeMm7fWtSTztTg-0MBScPQ%3D%3D?uid=0&filename=brunch.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v3&size=2048x2048",
+        imageUrl: "https://images.wallpaperscraft.ru/image/single/kakao_zefir_pled_118517_300x168.jpg",
         description: "описание вкусный завтрак в городе"
     },
 
